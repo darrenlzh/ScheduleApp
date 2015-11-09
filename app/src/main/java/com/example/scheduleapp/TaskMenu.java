0c801de129c0ab0dee4aa2ce6e841f568f32e085
Matthew <mjlease@buffalo.edu>
2015-11-09 18:35:52 -0500

    Adding Database functionality

--- app/src/main/java/com/example/scheduleapp/TaskMenu.java
+++ app/src/main/java/com/example/scheduleapp/TaskMenu.java
@@ -1,6 +1,7 @@
 package com.example.scheduleapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 public class TaskMenu extends AppCompatActivity {
 
@@ -23,8 +26,22 @@ public class TaskMenu extends AppCompatActivity {
 
     public void buttonAddTask(View view) {
         Button button = (Button) view;
-        String name;
-        findViewById(0);
+        String name, loc, desc;
+        if((name = findViewById(R.id.name_line).toString()) .equals("")){
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+                dlgAlert.setMessage("Name line must be filled.");
+                dlgAlert.setTitle("Error Message...");
+                dlgAlert.setPositiveButton("OK", null);
+                dlgAlert.setCancelable(true);
+                dlgAlert.create().show();
+                return;
+        }
+        loc = findViewById(R.id.location_line).toString();
+        desc = findViewById(R.id.description_line).toString();
+        DatePicker date = (DatePicker)findViewById(R.id.datePicker2);
+        TimePicker time = (TimePicker)findViewById(R.id.timePicker);
+
+
         MainActivity._db.insertData("", "", "", "", "");
     }
 }
