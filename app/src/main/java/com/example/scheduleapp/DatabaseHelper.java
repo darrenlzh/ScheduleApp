a8ac2d3ba61b0b6317c1e44eaac5183d6ee3348c
Matthew <mjlease@buffalo.edu>
2015-11-09 20:01:03 -0500

    TaskMenu

--- app/src/main/java/com/example/scheduleapp/DatabaseHelper.java
+++ app/src/main/java/com/example/scheduleapp/DatabaseHelper.java
@@ -5,26 +5,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Matthew on 11/9/2015.
- */
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Tasks";
     public static final String TABLE_NAME = "Task_List";
+    public static final String ID = "ID";
     public static final String TASK_NAME = "Task_Name";
     public static final String TASK_LOCATION = "Task_Location";
     public static final String TASK_DATE = "Task_Date";
     public static final String TASK_TIME = "Task_Time";
     public static final String TASK_DESC = "Task_Desc";
 
-    public DatabaseHelper(Context context) {//, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        //super(context, name, factory, version);
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (Task_Name TEXT PRIMARY KEY, Task_Location TEXT, Task_Desc TEXT, Task_Date TEXT, Task_Time TEXT");
+        String SQL_String = "CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Task_Name TEXT, Task_Location TEXT, Task_Desc TEXT, Task_Date TEXT, Task_Time TEXT";
+        db.execSQL(SQL_String);
     }
 
     @Override
@@ -41,8 +39,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         content.put(TASK_DESC, desc);
         content.put(TASK_DATE, date);
         content.put(TASK_TIME, time);
-        long result = db.insert(TABLE_NAME, null, content);
-        if(result == -1) { return false; }
+        long result;
+        if((result = db.insert(TABLE_NAME, null, content)) == -1) { return false; }
         return true;
     }
 }
