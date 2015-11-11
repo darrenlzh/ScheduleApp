f1487d9fc37a8dba3560f8960345ebabd5bc24c4
Darren Lim <darrenlim@dhcp095-242-192.wireless.buffalo.edu>
2015-11-11 16:31:19 -0500

    Adding functionality to TaskMenu
    Added the ability to choose ALL DAY when creating a reminder.

--- app/src/main/java/com/example/scheduleapp/TaskMenu.java
+++ app/src/main/java/com/example/scheduleapp/TaskMenu.java
@@ -1,18 +1,13 @@
 package com.example.scheduleapp;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ComponentName;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.speech.RecognitionListener;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -21,6 +16,8 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class TaskMenu extends AppCompatActivity {
+
+    public boolean dateAndTimeClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +45,15 @@ public class TaskMenu extends AppCompatActivity {
         loc = text.getText().toString();
         text = (EditText)findViewById(R.id.description);
         desc = text.getText().toString();
-        DatePicker dp = (DatePicker)findViewById(R.id.datePicker);
-        TimePicker tp = (TimePicker)findViewById(R.id.timePicker);
-        date = String.valueOf(dp.getDayOfMonth()) + " - " + String.valueOf(dp.getMonth() + 1) + " - " + String.valueOf(dp.getYear());
-        tp.clearFocus();
-        time = String.valueOf(tp.getCurrentHour()) + ":" + String.valueOf(tp.getCurrentMinute());
+        if(dateAndTimeClicked) {
+            DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
+            TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
+            date = String.valueOf(dp.getDayOfMonth()) + " - " + String.valueOf(dp.getMonth() + 1) + " - " + String.valueOf(dp.getYear());
+            tp.clearFocus();
+            time = String.valueOf(tp.getCurrentHour()) + ":" + String.valueOf(tp.getCurrentMinute());
+        } else {
+            date =""; time = "";
+        }
         DatabaseHelper db = DatabaseHelper.getInstance(getApplicationContext());
         if(db.insertData(name, loc, desc, date, time)) {
             Toast.makeText(TaskMenu.this, R.string.taskAdded, Toast.LENGTH_LONG).show();
@@ -64,13 +65,36 @@ public class TaskMenu extends AppCompatActivity {
     }
 
     public void buttonDateAndTimeContainer(View view) {
+        dateAndTimeClicked = true;
         View date = (View) findViewById(R.id.datePicker);
         View time = (View) findViewById(R.id.timePicker);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkAllDay);
         View description = (View) findViewById(R.id.description);
         RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams) description.getLayoutParams();
-        p.addRule(RelativeLayout.BELOW, R.id.timePicker);
+        p.addRule(RelativeLayout.BELOW, R.id.checkAllDay);
         description.setLayoutParams(p);
         date.setVisibility(View.VISIBLE);
         time.setVisibility(View.VISIBLE);
+        checkBox.setVisibility(View.VISIBLE);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                boolean timeIsVisible = true;
+                View time = (View) findViewById(R.id.timePicker);
+                CheckBox checkBox = (CheckBox) findViewById(R.id.checkAllDay);
+                RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) checkBox.getLayoutParams();
+                if (isChecked) {
+                    lp.addRule(RelativeLayout.BELOW, R.id.datePicker);
+                    checkBox.setLayoutParams(lp);
+                    time.setVisibility(View.INVISIBLE);
+                } else {
+                    lp.addRule(RelativeLayout.BELOW, R.id.timePicker);
+                    time.setVisibility(View.VISIBLE);
+                    checkBox.setLayoutParams(lp);
+                }
+
+            }
+        });
     }
 }
