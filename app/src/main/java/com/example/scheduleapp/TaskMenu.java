c6909100e11717cc554d6818c1b01af4171e7dc3
Matthew <mjlease@buffalo.edu>
2015-11-11 21:41:23 -0500

    Finished

--- app/src/main/java/com/example/scheduleapp/TaskMenu.java
+++ app/src/main/java/com/example/scheduleapp/TaskMenu.java
@@ -54,12 +54,15 @@ public class TaskMenu extends AppCompatActivity {
             TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
             date = String.valueOf(dp.getDayOfMonth()) + " - " + String.valueOf(dp.getMonth() + 1) + " - " + String.valueOf(dp.getYear());
             tp.clearFocus();
-            time = String.valueOf(tp.getCurrentHour()) + ":";
+            String hour = String.valueOf(tp.getCurrentHour()) + ":";
             String minute = String.valueOf(tp.getCurrentMinute());
+            if(hour.length() == 1) {
+                hour = hour + "0";
+            }
             if(minute.length() == 1) {
                 minute = minute + "0";
             }
-            time = time + minute;
+            time = hour + ":" + minute;
         } else {
             date =""; time = "";
         }
