ec1ba133de394586c80fe0d35e9d0c0d312cda87
Darren Lim <darrenlim@dhcp095-242-192.wireless.buffalo.edu>
2015-11-11 20:15:56 -0500

    Updating day view layout
    updated

--- app/src/main/java/com/example/scheduleapp/DayView.java
+++ app/src/main/java/com/example/scheduleapp/DayView.java
@@ -22,13 +22,15 @@ public class DayView extends Fragment {
         View inflatedView = inflater.inflate(R.layout.dayview, container, false);
 
         TextView dayOfMonth = (TextView) inflatedView.findViewById(R.id.dayOfMonth);
-        TextView monthName = (TextView) inflatedView.findViewById(R.id.monthName);
+        TextView monthName = (TextView) inflatedView.findViewById(R.id.dayOfWeek);
         final Calendar calendar = Calendar.getInstance();
         SimpleDateFormat format = new SimpleDateFormat("MMM");
+        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
         String month = format.format(calendar.getTime());
+        String day = dayFormat.format(calendar.getTime());
         int mDay = calendar.get(Calendar.DAY_OF_MONTH);
         dayOfMonth.setText(mDay+"");
-        monthName.setText(month);
+        monthName.setText(day);
 
         return inflatedView;
     }
